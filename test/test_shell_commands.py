a6897ea5ebd126035219a70fd2e686b64c07cdb5
Simon Johnston <simonjo@amazon.com>
2018-03-05 15:29:01 -0800

    Getting close to having Travis work

--- test/test_shell_commands.py
+++ test/test_shell_commands.py
@@ -1,2 +1,11 @@
-def test_placeholder():
+from rdftools.scripts import shell
+
+def test_commands_added():
+    for cmd in ['base', 'help', 'exit']:
+        assert cmd in shell.COMMANDS
+
+def test_command_help():
+    pass
+
+def test_command_completion():
     pass
